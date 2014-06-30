8ca210533707dd0e42f1c898d69eac87300ae5af
Felix <felix@Felixs-MacBook-Pro.local>
2014-06-30 14:38:12 +0100

    updated migrations

--- db/migrate/20140630124702_rename_drinks_to_products.rb
+++
@@ -1,5 +0,0 @@
-class RenameProductsToProducts < ActiveRecord::Migration
-  def change
-  	rename_table :products, :products
-  end
-end
--- db/migrate/20140630125948_change_salesto_product_i_ds.rb
+++
@@ -1,5 +0,0 @@
-class ChangeSalestoProductIDs < ActiveRecord::Migration
-  def change
-  	rename_column :sales, :drink_id, :product_id
-  end
-end
