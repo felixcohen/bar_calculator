8ccf1902c2c5ca3125da8a9bcb5f37be8c73ec0e
Felix <felix@Felixs-MacBook-Pro.local>
2014-07-04 15:47:06 +0100

    js and styles and GA

--- app/assets/javascripts/discounts.js
+++ app/assets/javascripts/discounts.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+	setTimeout(function() {
+	  $(".alert").slideUp();
+	}, 2500);
+	$('.close').click(function() {
+	  $(".alert").slideUp();
+	})
+})
